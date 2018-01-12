c9fbcdd257467e6d1b306db82f7545a6dc30e309
Moudy <mood93@Mahmouds-MacBook-Air.local>
2018-01-12 15:51:45 -0600

    v1.4
    Add Jumbotron
    Add rows w/flex

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
     {name: "McFadden Cove", image: "https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"},
     {name: "Coon Creek", image: "https://farm4.staticflickr.com/3742/10759552364_a796a5560a.jpg"},
+    {name: "Washunga Bay", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},{name: "McFadden Cove", image: "https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"},
+    {name: "Coon Creek", image: "https://farm4.staticflickr.com/3742/10759552364_a796a5560a.jpg"},
+    {name: "Washunga Bay", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},    {name: "McFadden Cove", image: "https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"},
+    {name: "Coon Creek", image: "https://farm4.staticflickr.com/3742/10759552364_a796a5560a.jpg"},
+    {name: "Washunga Bay", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"},{name: "McFadden Cove", image: "https://farm9.staticflickr.com/8167/7121865553_e1c6a31f07.jpg"},
+    {name: "Coon Creek", image: "https://farm4.staticflickr.com/3742/10759552364_a796a5560a.jpg"},
     {name: "Washunga Bay", image: "https://farm3.staticflickr.com/2931/14128269785_f27fb630f3.jpg"}
 ]
 
